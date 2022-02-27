350f1f483bdc777795aeb2ecb6ab4d39ffe095fb
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-27 10:47:02 +0200

    davinci: Add dummy AIDL vibrator HAL
    Change-Id: I3e1743a6f9a54f4b53a63060e62a6e684eebeae7

--- device.mk
+++ device.mk
@@ -209,6 +209,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator.service.davinci
+
 # WLAN
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wlan/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/qca6490/WCNSS_qcom_cfg.ini
