10b00db62e5ead09f60963e1c96255ba26908d48
Alexander Koskovich <akoskovich@pm.me>
2022-09-20 18:10:19 -0700

    davinci: commit history is for losers
    Change-Id: I0c18e2be77a1e93a4ee21c88b2b794d12db2c2b7

--- device.mk
+++ device.mk
@@ -4,24 +4,88 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from our proprietary files directory.
+$(call inherit-product, vendor/asus/davinci/davinci-vendor.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=erofs \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 PRODUCT_PACKAGES += \
+    checkpoint_gc \
     otapreopt_script
+
+# APEX
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Bluetooth
+PRODUCT_SOONG_NAMESPACES += \
+    packages/modules/Bluetooth/android/app
 
 # Board
 TARGET_BOARD_PLATFORM := taro
 
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Fastboot
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+
+PRODUCT_PACKAGES += \
+    fstab.qcom
+
 # Kernel
-include vendor/qcom/proprietary/kernel-platform/kernel-platform-product.mk
-include vendor/qcom/proprietary/kernel-platform/kernel-platform-board.mk
+KERNEL_VARIABLES := ASUS_BUILD_PROJECT=AI2202
+KERNEL_VARIANT := gki
+
+# Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Project ID Quota
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# QTI Components
+TARGET_COMMON_QTI_COMPONENTS := \
+    display \
+    init
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.1-service.multihal \
+    libsensorndkbridge
+
+# Storage
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Shipping API
+BOARD_API_LEVEL := 31
+BOARD_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
+
+# Update Engine
+PRODUCT_PACKAGES += \
+    update_engine_sideload
--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2022 Paranoid Android
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-export ASUS_BUILD_PROJECT=AI2202
-export ASUS_GKI_BUILD=y
-export KERNEL_VARIANT=gki
