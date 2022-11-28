5a45fe62e95f3d0368594f4552875f248de539f7
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-11-28 14:12:14 -0500

    davinci: Get NFC rolling hopefully

--- device.mk
+++ device.mk
@@ -254,7 +254,10 @@ PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.ccodec=4
 NEED_AIDL_NDK_PLATFORM_BACKEND := true
 
 # NFC
-TARGET_USES_NQ_NFC := false
+$(call inherit-product, vendor/nxp/nfc/nfc-vendor-product.mk)
+$(call inherit-product, vendor/nxp/secure_element/se-vendor-product.mk)
+
+PRODUCT_USES_ESE := false
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
@@ -262,7 +265,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
+    frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_eSE/android.hardware.nfc.ese.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_eSE/android.hardware.se.omapi.ese.xml
 
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
