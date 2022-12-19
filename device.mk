16fe746e1275075b578e48b083725cd1c43f6a02
Sourajit Karmakar <sourajitk@statixos.com>
2022-12-18 21:17:56 -0500

    davinci: Revert vibrator related commits
    Reason for revert: Davinci does NOT use aw8697 instead relying
    on QTI haptics. It was a trival mistake but since the motors
    themeselves are identical, I assumed they used the same drivers.
    
    Squashed commit of the following:
    
    commit 00808ca786474dd9dad92477a1eb6b9a8e83c3a7
    Author: Sourajit Karmakar <sourajitk@statixos.com>
    Date:   Sun Dec 18 21:17:40 2022 -0500
    
        Revert "davinci: Add dummy AIDL vibrator HAL"
    
        This reverts commit 350f1f483bdc777795aeb2ecb6ab4d39ffe095fb.
    
    commit 5465b61339e019044e4693677809f142ff4f5522
    Author: Sourajit Karmakar <sourajitk@statixos.com>
    Date:   Sun Dec 18 21:17:11 2022 -0500
    
        Revert "davinci: vibrator: Implement using AW8697 attributes"
    
        This reverts commit 2b8243ccf1f71ec8f7e7845f898cfc002be4d653.
    
    commit 62b5b8b655c3a86b34593a07e83629fc6387d25a
    Author: Sourajit Karmakar <sourajitk@statixos.com>
    Date:   Sun Dec 18 21:17:04 2022 -0500
    
        Revert "davinci: Remove ndk_platform backend. Use the ndk backend"
    
        This reverts commit 21918ae2cd46c54243d82f6673b00895264491cb.

--- device.mk
+++ device.mk
@@ -422,10 +422,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator.service.davinci
-
 # WLAN
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
